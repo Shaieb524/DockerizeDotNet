69cf20679e1ed0a723261627ed1522c214443631
Joud Shaieb <joudshaieb524@gmail.com>
2025-03-21 23:55:32 +0400

    dev: add context asnd mirgrations

---
+++ WebApiDb/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApiDb.Entities;
+
+namespace WebApiDb.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    }
+}
---
+++ WebApiDb/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration; 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WebApiDb.Context
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "DotNetApi");
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
